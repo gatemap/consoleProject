bec545abb81c6222ff10fb07069edebf2a22c962
gatemap <38553075+gatemap@users.noreply.github.com>
2020-09-10 19:02:01 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using IrrKlang;
+using static System.Console;
+
+namespace consoleProejct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Test");
+            ReadLine();
+        }
+    }
+}
